c2f202b8e05574e3303ccd35196cd836042674d0
debop <sunghyouk.bae@gmail.com>
2013-03-19 20:39:45 +0900

    대소문자 변경을 위해

--- vital-domain/src/main/java/com/kt/vital/domain/repository/VoccRepository.java
+++ vital-domain/src/main/java/com/kt/vital/domain/repository/VoccRepository.java
@@ -12,9 +12,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 @Slf4j
-public class VoCRepository extends HibernateRepository<Voc> {
+public class VoccRepository extends HibernateRepository<Voc> {
 
-    public VoCRepository() {
+    public VoccRepository() {
         super(Voc.class);
     }
 }
