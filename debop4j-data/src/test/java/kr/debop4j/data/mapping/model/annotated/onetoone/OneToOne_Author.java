55aaa454107e9bb15bd37f8bcf6b885b3530590b
debop <sunghyouk.bae@gmail.com>
2013-03-23 22:20:22 +0900

    one-to-one 매핑 추가

---
+++ debop4j-data/src/test/java/kr/debop4j/data/mapping/model/annotated/onetoone/OneToOne_Author.java
@@ -0,0 +1,39 @@
+package kr.debop4j.data.mapping.model.annotated.onetoone;
+
+import kr.debop4j.core.tools.HashTool;
+import kr.debop4j.data.model.AnnotatedEntityBase;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * kr.debop4j.data.mapping.model.annotated.onetoone.OneToOne_Author
+ *
+ * @author sunghyouk.bae@gmail.com
+ *         13. 3. 23. 오후 10:08
+ */
+@Entity
+@Table(name = "OneToOne_Author")
+@Getter
+@Setter
+public class OneToOne_Author extends AnnotatedEntityBase {
+
+    private static final long serialVersionUID = 7068044349471017755L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "AuthorId")
+    private Long id;
+
+    @Column(name = "AuthorName", nullable = false, length = 128)
+    private String name;
+
+    @OneToOne(mappedBy = "author", cascade = {CascadeType.ALL})
+    private OneToOne_Biography biography;
+
+    @Override
+    public int hashCode() {
+        return (isPersisted()) ? HashTool.compute(id) : HashTool.compute(name);
+    }
+}
---
+++ debop4j-data/src/test/java/kr/debop4j/data/mapping/model/annotated/onetoone/OneToOne_Biography.java
@@ -0,0 +1,41 @@
+package kr.debop4j.data.mapping.model.annotated.onetoone;
+
+import kr.debop4j.data.model.AnnotatedEntityBase;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+
+/**
+ * kr.debop4j.data.mapping.model.annotated.onetoone.OneToOne_Biography
+ *
+ * @author sunghyouk.bae@gmail.com
+ *         13. 3. 23. 오후 10:08
+ */
+@Entity
+@Table(name = "OneToOne_Biography")
+@Getter
+@Setter
+public class OneToOne_Biography extends AnnotatedEntityBase {
+
+    private static final long serialVersionUID = -5261462229916986070L;
+
+    /**
+     * Hibernate 기본의 one-to-one 입니다!!!
+     */
+    @Id
+    @GeneratedValue(generator = "gen")
+    @GenericGenerator(name = "gen", strategy = "foreign",
+                      parameters = @Parameter(name = "property", value = "author"))
+    @Column(name = "AuthorId")
+    private Long authorId;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private OneToOne_Author author;
+
+    @Column(name = "Information", length = 1024)
+    private String information;
+}
