9d7fa4debd2b6579ed09760e94ce5c2bd4f970f8
sunghyouk.bae@gmail.com <@baekwon9>
2013-03-27 18:16:16 +0900

    nosql 관련 코드를 모두 별개의 모듈로 분리함.

---
+++ debop4j-nosql-memcached/src/main/java/kr/debop4j/nosql/memcached/MemcachedCache.java
@@ -0,0 +1,86 @@
+package kr.debop4j.nosql.memcached;
+
+import kr.debop4j.core.Guard;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.internal.GetFuture;
+import net.spy.memcached.internal.OperationFuture;
+import org.springframework.cache.Cache;
+import org.springframework.cache.support.SimpleValueWrapper;
+
+/**
+ * Memcached를 캐시 저장소로 사용하는 Cache
+ *
+ * @author sunghyouk.bae@gmail.com
+ *         Date: 13. 3. 25 오전 11:42
+ */
+@Slf4j
+public class MemcachedCache implements Cache {
+
+    @Getter
+    private String name = "default";
+    @Getter
+    private MemcachedClient nativeCache = null;
+
+    @Getter
+    private int expireSeconds;
+
+    public MemcachedCache(MemcachedClient client) {
+        this(client, 26 * 60 * 60);
+    }
+
+    public MemcachedCache(MemcachedClient client, int expireSeconds) {
+        Guard.shouldNotBeNull(client, "client");
+
+        this.nativeCache = client;
+        this.expireSeconds = expireSeconds;
+        if (log.isDebugEnabled())
+            log.debug("MemcachedCache를 생성했습니다");
+    }
+
+    @Override
+    public ValueWrapper get(Object key) {
+        Guard.shouldNotBeNull(key, "key");
+
+        GetFuture<Object> result = nativeCache.asyncGet(key.toString());
+
+        SimpleValueWrapper wrapper = null;
+        try {
+            if (result.get() != null)
+                wrapper = new SimpleValueWrapper(result.get());
+        } catch (Exception ignored) {}
+        return wrapper;
+    }
+
+    @Override
+    public void put(Object key, Object value) {
+        Guard.shouldNotBeNull(key, "key");
+        if (!(key instanceof String)) {
+            log.error("Invalid key type: " + key.getClass());
+            return;
+        }
+
+        OperationFuture<Boolean> setOp = null;
+        setOp = nativeCache.set(key.toString(), expireSeconds, value);
+
+        if (log.isInfoEnabled()) {
+            if (setOp.getStatus().isSuccess()) {
+                log.info("객체를 캐시 키[{}]로 저장했습니다.", key);
+            } else {
+                log.info("객체를 캐시 키[{}]로 저장하는데 실패했습니다. operation=[{}]", key, setOp);
+            }
+        }
+    }
+
+    @Override
+    public void evict(Object key) {
+        if (key != null)
+            nativeCache.delete(key.toString());
+    }
+
+    @Override
+    public void clear() {
+        nativeCache.flush();
+    }
+}
