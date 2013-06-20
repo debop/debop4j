57a70741225d885fc8a28d4e1d0d94e1132c7370
debop <sunghyouk.bae@gmail.com>
2013-06-20 22:54:44 +0900

    LabmdaJ 제거, Slf4j 버전 1.7.5 로 변경

--- debop4j-core/src/test/java/kr/debop4j/core/cryptography/JasyptTest.java
+++ debop4j-core/src/test/java/kr/debop4j/core/cryptography/JasyptTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kr.debop4j.core.cryptography;
 
 import com.google.common.collect.Lists;
@@ -15,8 +31,8 @@ import java.security.Provider;
 import java.security.Security;
 import java.util.List;
 
-import static ch.lambdaj.Lambda.on;
-import static ch.lambdaj.Lambda.sort;
+// import static ch.lambdaj.Lambda.on;
+// import static ch.lambdaj.Lambda.sort;
 
 /**
  * jasypt.org 테스트
@@ -43,7 +59,7 @@ public class JasyptTest {
     public void loadMessageDigest() {
         final List<String> algos = Lists.newArrayList(Security.getAlgorithms("MessageDigest"));
 
-        for (Object algorithm : sort(algos, on(String.class))) {
+        for (Object algorithm : algos) { // sort(algos, on(String.class))) {
             log.debug("MessageDigest Algorithm=[{}]", algorithm);
         }
     }
@@ -51,7 +67,7 @@ public class JasyptTest {
     @Test
     public void loadCiphers() {
         final List<String> algos = Lists.newArrayList(Security.getAlgorithms("Cipher"));
-        for (Object algorithm : sort(algos, on(String.class))) {
+        for (Object algorithm : algos) { // sort(algos, on(String.class))) {
             log.debug("Symethric Algorithm=[{}]", algorithm);
         }
     }
